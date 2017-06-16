a223851fe4ad5d0f66e84d6c7a66697d65a98095
Miao123 <miao_ios@163.comc>
2017-06-16 19:46:55 +0800

    Initial Commit

---
+++ 弹出框/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  弹出框
+//
+//  Created by 苗建浩 on 2017/6/16.
+//  Copyright © 2017年 苗建浩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 弹出框/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  弹出框
+//
+//  Created by 苗建浩 on 2017/6/16.
+//  Copyright © 2017年 苗建浩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
